dec024695b2f6e1a732a8708f17f0d5bda4ad3b3
Caleb Rivera <calebrossr@gmail.com>
2024-01-27 06:09:46 -0500

    update php files

---
+++ src/api/sign-in.php
@@ -0,0 +1,54 @@
+<?php
+$body = get_request_body();
+
+$login = $body['login'];
+$password = $body['password'];
+
+$db_host = '';
+$db_user = '';
+$db_password = '';
+$db_name = '';
+
+$connection = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+if ($connection->connect_error) {
+    send_json(json_encode(array('error' => $connection->connect_error)));
+    return;
+}
+
+$query = "SELECT * FROM USERS WHERE LOGIN = ?";
+$statement = $connection->prepare($query);
+$statement->bind_param('s', $body['login']);
+$statement->execute();
+
+$result = $statement->get_result();
+
+if ($result->num_rows == 0) {
+    send_json(json_encode(array('error' => 'No user found')));
+    return;
+}
+
+$row = $result->fetch_assoc();
+$hashedPassword = $row['PASSWORD'];
+
+if (!password_verify($password, $hashedPassword)) {
+    send_json(json_encode(array('error' => 'Incorrect login or password')));
+    return;
+}
+
+send_json(json_encode(array('id' => $row['INTNUM'])));
+
+$statement->close();
+$connection->close();
+
+
+function get_request_body()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function send_json($object)
+{
+    header('Content-type: application/json');
+    echo $object;
+}
---
+++ src/api/sign-up.php
@@ -0,0 +1,44 @@
+<?php
+$body = get_request_body();
+
+$first = $body['first'];
+$last = $body['last'];
+$email = $body['email'];
+$phone = $body['phone'];
+$login = $body['login'];
+$password = password_hash($body['password'], PASSWORD_DEFAULT);
+
+$db_host = '';
+$db_user = '';
+$db_password = '';
+$db_name = '';
+
+$connection = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+if ($connection->connect_error) {
+    send_json(json_encode(array('error' => $connection->connect_error)));
+}
+
+$query = "INSERT INTO USERS (FIRST,LAST,EMAIL,PHONE,LOGIN,PASSWORD) VALUES (?,?,?,?,?,?)";
+
+$statement = $connection->prepare($query);
+$statement->bind_param('ssssss', $first, $last, $email, $phone, $login, $password);
+$statement->execute();
+
+$id = $connection->insert_id;
+
+send_json(json_encode(array('id' => $id)));
+
+$statement->close();
+$connection->close();
+
+function get_request_body()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function send_json($object)
+{
+    header('Content-type: application/json');
+    echo $object;
+}
