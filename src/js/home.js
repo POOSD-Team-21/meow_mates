641aebe11d8bdca4fe80ff00efe9cd400c655472
Conner Harbaugh <savvydolphin77@gmail.com>
2024-01-27 12:16:17 -0500

    Hides Signup When Logged In & Renames index.js
    Renames index.js to home.js to match home page. Added comments to js file. Updated home.html to reference new name for js. Added logic to hide the signup button when user is logged in.

--- src/js/home.js
+++ src/js/home.js
@@ -1,16 +1,29 @@
+// gets user to see if we are logged in
 const user = JSON.parse(localStorage.getItem('user'));
 
+// sets ids for for the HTML elements
 const signInLink = document.querySelector('#sign-in-link');
 const signOutButton = document.querySelector('#sign-out-button');
+const signUpButton = document.querySelector('#sign-up-button');
 
+// Edits customization for when user is logged in or not
 if (user) {
+  // swaps sign in button with sign out button
   signInLink.style.display = 'none';
   signOutButton.style.display = 'block';
+
+  // hides the sign up button on main page
+  signUpButton.style.display = 'none';
 } else {
+  // user is not logged in, so we show the sign-in button
   signInLink.style.display = 'block';
   signOutButton.style.display = 'none';
+
+  // shows the sign up button on main page
+  signUpButton.style.display = 'block';
 }
 
+// signs user out
 signOutButton.addEventListener('click', () => {
   localStorage.removeItem('user');
   window.location.href = '/sign-in.html';
