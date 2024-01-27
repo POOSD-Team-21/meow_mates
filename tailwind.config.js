394ae738c9417ba45b473a4f39ca5dfd438e6f5d
Conner Harbaugh <savvydolphin77@gmail.com>
2024-01-26 21:03:42 -0500

    Update index.html With Color Palette
    Added color palette to main config. Updated index.html page with new color schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ['**/*.html'],
   theme: {
     extend: {},
+    // Colors for Color Palette 
+    colors: {
+      'main-background-color': '#CAFCF6',
+      'main-text-color': '#163325',
+    }
   },
   plugins: [],
 };
