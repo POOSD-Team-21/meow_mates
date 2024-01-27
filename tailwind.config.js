25061806afc1459c2670651d3a3f2646dfff3287
Conner Harbaugh <savvydolphin77@gmail.com>
2024-01-27 10:40:23 -0500

    Ran prettier to Fix Styling
    Made styling consistent by running prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@ module.exports = {
   content: ['**/*.html'],
   theme: {
     extend: {},
-    // Colors for Color Palette 
+    // Colors for Color Palette
     colors: {
       'main-background-color': '#CAFCF6',
       'main-text-color': '#163325',
-    }
+    },
   },
   plugins: [],
 };
