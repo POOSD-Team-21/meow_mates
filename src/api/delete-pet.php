f257f3420e89cb2e55065c95a485a50658ba6cdf
Sergio Cardona <sergio17cardona@gmail.com>
2024-01-30 19:04:08 -0500

    updates 2 enpoints
    signup now checks for suername and fixed delte bugs now returning b etter json

--- src/api/delete-pet.php
+++ src/api/delete-pet.php
@@ -1,63 +1,62 @@
 <?php
 
-	loadEnv(__DIR__.'/../.env');
+loadEnv(__DIR__.'/../.env');
 
-	$host = $_ENV['DB_HOST'];
-	$database = $_ENV['DB_DATABASE'];
-	$user = $_ENV['DB_USERNAME'];
-	$dbPassword = $_ENV['DB_PASSWORD'];
+$host = $_ENV['DB_HOST'];
+$database = $_ENV['DB_DATABASE'];
+$user = $_ENV['DB_USERNAME'];
+$dbPassword = $_ENV['DB_PASSWORD'];
 
-	$inData = getRequestInfo();
-	
-	$intnum = $inData['id'];
+$inData = getRequestInfo();
 
-	
-	$searchResults = "";
-	$searchCount = 0;
+$intnum = $inData['id'];
 
-	$conn = new mysqli($host, $user, $dbpassword, $database);
+$conn = new mysqli($host, $user, $dbPassword, $database);
 
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$query = "DELETE FROM PETS WHERE INTNUM = ?";
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
+    $query = "DELETE FROM PETS WHERE INTNUM = ?";
+    $stmt = $conn->prepare($query);
+    
+    if ($stmt) {
+        $stmt->bind_param('i', $intnum);
+        $stmt->execute();
+        
+        $deletedPetId = $conn->insert_id; // Get the ID of the deleted pet
+        
+        $stmt->close();
+        $conn->close();
+        
+        returnWithSuccess($deletedPetId);
+    } else {
+        returnWithError("Delete statement preparation error: " . $conn->error);
+    }
+}
 
-		$stmt = $conn->prepare($query);
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
 
-		$stmt->bind_param('i',$intnum);
-		$stmt->execute();
-	
-		$stmt->close();
-		$conn->close();
-		
-	}
-		
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo json_encode($obj);
+}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo($firstName, $lastName, $id)
-	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson($retValue);
-	}
-	
+function returnWithError($err)
+{
+    $retValue = ['error' => $err];
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithSuccess($deletedPetId)
+{
+    $retValue = ['message' => 'Pet deleted successfully', 'deleted_pet_id' => $deletedPetId];
+    sendResultInfoAsJson($retValue);
+}
+
 function loadEnv($filePath)
 {
     if (!file_exists($filePath)) {
