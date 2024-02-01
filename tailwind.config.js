be3459ea51512d30d2be7fbdcc1723f6c86ab4e4
Caleb Rivera <calebrossr@gmail.com>
2024-01-31 23:42:01 -0500

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   content: ['**/*.html'],
   theme: {
-    extend: {},
-    // Colors for Color Palette
-    colors: {
-      'main-background-color': '#CAFCF6',
-      'main-text-color': '#163325',
+    extend: {
+      colors: {
+        'main-background-color': '#CAFCF6',
+        'main-text-color': '#163325',
+      },
     },
+    // Colors for Color Palette
   },
   plugins: [],
 };
