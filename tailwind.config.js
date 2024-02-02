26fbafdddf4446e73992cba7d8ea68466e3c2232
Caleb Rivera <calebrossr@gmail.com>
2024-02-02 07:46:17 -0500

    add tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  content: ['**/*.html'],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
+      // Colors for Color Palette
       colors: {
         'main-background-color': '#CAFCF6',
         'main-text-color': '#163325',
       },
     },
-    // Colors for Color Palette
   },
-  plugins: [],
+  plugins: [require('tailwindcss-3d'), require('tailwindcss-animate')],
 };
